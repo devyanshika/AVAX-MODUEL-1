f30f399b53e11e297e97ebd08a3defd3ec48eaa8
devyanshika <106855016+devyanshika@users.noreply.github.com>
2024-09-30 01:32:50 +0530

    Vault.sol

--- Vault.sol
+++ Vault.sol
@@ -1,1 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract Vault is ReentrancyGuard {
+    IERC20 public immutable token;
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    event Deposit(address indexed user, uint amount, uint shares);
+    event Withdraw(address indexed user, uint amount, uint shares);
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function _mint(address _to, uint _shares) private {
+        totalSupply += _shares;
+        balanceOf[_to] += _shares;
+    }
+
+    function _burn(address _from, uint _shares) private {
+        totalSupply -= _shares;
+        balanceOf[_from] -= _shares;
+    }
+
+    function deposit(uint _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        
+        uint shares;
+        if (totalSupply == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+
+        _mint(msg.sender, shares);
+        require(token.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+
+        emit Deposit(msg.sender, _amount, shares);
+    }
+
+    function withdraw(uint _shares) external nonReentrant {
+        require(balanceOf[msg.sender] >= _shares, "Insufficient shares");
+        
+        uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        _burn(msg.sender, _shares);
+        require(token.transfer(msg.sender, amount), "Transfer failed");
+
+        emit Withdraw(msg.sender, amount, _shares);
+    }
+}
 

ba68f3b4fc34b319b4077fd2bd03de79c72feb06
devyanshika <106855016+devyanshika@users.noreply.github.com>
2024-09-30 01:31:57 +0530

    MySub.sol

--- MySub.sol
+++ MySub.sol
@@ -1,1 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract UniqueERC20 {
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Mint(address indexed to, uint amount);
+    event Burn(address indexed from, uint amount);
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        mint(_initialSupply);
+    }
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool) {
+        require(balanceOf[sender] >= amount, "Insufficient balance");
+        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded");
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint amount) internal {
+        require(amount > 0, "Amount must be greater than 0");
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Mint(msg.sender, amount);
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Burn(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
 
